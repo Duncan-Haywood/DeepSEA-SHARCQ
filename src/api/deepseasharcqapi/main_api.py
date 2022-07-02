e63a2613b5808a11849bba1bfb9dfdaf172c5da2
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-07-01 19:58:42 -0600

    combined files

--- src/api/deepseasharcqapi/main_api.py
+++ src/api/deepseasharcqapi/main_api.py
@@ -5,35 +5,40 @@ from typing import List
 from mangum import Mangum
 from .aws_util import AWSUtil
 import uvicorn
+from asgiref.typing import ASGIApplication
 
 
 class PredictBody(BaseModel):
     user: str
     email: str
 
-    
-app = FastAPI()
+def create_api():
+    app = FastAPI()
 
+    @app.post('/predict/')
+    async def predict(files: List[UploadFile], body: PredictBody):
+        #create folder path and awsUtil obj
+        time = str(datetime.now())
+        folder_path = f'{body.user}_{time}'
+        aws_util = AWSUtil(folder_path)
+        #upload to s3 - TODO might need more parallelization or memory management ie in parts for upload - might need some errror handling for uploads greater than ~7 GB (~175 files) (10 gb max on lambda)
+        aws_util.s3_upload_files(files)
+        #run ai application and close it when done
+        aws_util.exec_predict_ecs()
 
+        return {'results': 'we will email you with a signed url to the  predictions at the email you provided. You can download the result files with this.'} # maybe they need to do a get request at this fast api server when they have the url. TODO
 
-@app.post('/predict/')
-async def predict(files: List[UploadFile], body: PredictBody):
-    #create folder path and awsUtil obj
-    time = str(datetime.now())
-    folder_path = f'{body.user}_{time}'
-    aws_util = AWSUtil(folder_path)
-    #upload to s3 - TODO might need more parallelization or memory management ie in parts for upload - might need some errror handling for uploads greater than ~7 GB (~175 files) (10 gb max on lambda)
-    aws_util.s3_upload_files(files)
-    #run ai application and close it when done
-    aws_util.exec_predict_ecs()
+    # allows usage on aws lambda
+    handler = Mangum(app)
 
-    return {'results': 'we will email you with a signed url to the  predictions at the email you provided. You can download the result files with this.'} # maybe they need to do a get request at this fast api server when they have the url. TODO
+    return app, handler
 
-# allows usage on aws lambda
-handler = Mangum(app)
+def run_server(app: ASGIApplication):
+    uvicorn.run(app, port=5000, log_level="info")
 
-def run_server():
-    uvicorn.run("main_api:app", port=5000, log_level="info")
+def main():
+    app, handler = create_api()
+    run_server(app)
 
 if __name__ == '__main__':
-    run_server()
+    main()
--- src/api/tests/__main__test.py
+++
@@ -1,4 +0,0 @@
-from deepseasharcqapi import __main__ as main
-def test_main():
-    main.main()
-    #todo check if working
