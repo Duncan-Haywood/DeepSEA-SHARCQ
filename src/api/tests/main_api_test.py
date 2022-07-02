e773dd0293de24ce70003752cc00d6241fd76b9c
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-07-01 19:59:18 -0600

    adds tests for fast api server -- not working ; will run indefinitely

---
+++ src/api/tests/main_api_test.py
@@ -0,0 +1,14 @@
+from deepseasharcqapi import main_api
+from fastapi import FastAPI
+
+def test_main():
+    main_api.main()
+    #TODO check results
+
+def test_create_api():
+    main_api.create_api()
+    #TODO check results
+
+def test_run_server():
+    app = FastAPI()
+    main_api.run_server(app)
