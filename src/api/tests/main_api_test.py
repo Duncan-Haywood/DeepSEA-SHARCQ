709dbaa50d371b5a0b7094b21eb7ed6e1a681cf1
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-07-05 16:36:50 -0600

    comments

--- src/api/tests/main_api_test.py
+++ src/api/tests/main_api_test.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 def test_main():
     main_api.main()
     #TODO check results
+    #TODO close server
 
 def test_create_api():
     main_api.create_api()
@@ -12,3 +13,4 @@ def test_create_api():
 def test_run_server():
     app = FastAPI()
     main_api.run_server(app)
+    #TODO check results and close server
