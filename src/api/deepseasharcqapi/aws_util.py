8956d75fee4586c96c644a495ed8d490a1c5f0f5
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-07-05 16:35:27 -0600

    add aws utils

--- src/api/deepseasharcqapi/aws_util.py
+++ src/api/deepseasharcqapi/aws_util.py
@@ -3,6 +3,7 @@ from typing import List
 import boto3
 import os
 from joblib import Parallel, delayed
+import logging
 
 class AWSUtil:
     def __init__(self, folder_path: str):
@@ -17,17 +18,45 @@ class AWSUtil:
         # action to take in parallel 
         def _upload_file(file, key):
             with open(file.file) as f:
-                key = f'/uploads/{self.folder_path}/{file.filename}'
+                key = f'{self.folder_path}/{file.filename}'
                 # below should implement parallel multipart upload in theory
                 self.s3_bucket.upload_fileobj(f,key)
         # runs file uploads in parallel across all cpus - TODO hopefully it doesn't overtax the RAM (10 GB max on lambda instances) (jobs should be about 6 at max on lambda)
         Parallel(n_jobs=-1)(delayed(_upload_file)(file) for file in files)
             
     def launch_ai_ecs(self):    
-        raise NotImplementedError
+        return NotImplementedError
     def exec_predict_ecs(self):
-        raise NotImplementedError
+        return NotImplementedError
     def close_ai_ecs(self):
-        raise NotImplementedError
-    def s3_download_results(self):
-        raise NotImplementedError
+        return NotImplementedError
+    def s3_send_results(self):
+        return NotImplementedError
+
+class BucketWrapper:
+    def __init__(self, bucket):
+        self.bucket = bucket
+        self.name = bucket.name
+
+    def generate_presigned_post(self, object_key, expires_in):
+        """
+        Generate a presigned Amazon S3 POST request to upload a file.
+        A presigned POST can be used for a limited time to let someone without an AWS
+        account upload a file to a bucket.
+
+        :param object_key: The object key to identify the uploaded object.
+        :param expires_in: The number of seconds the presigned POST is valid.
+        :return: A dictionary that contains the URL and form fields that contain
+                 required access data.
+        """
+        try:
+            response = self.bucket.meta.client.generate_presigned_post(
+                Bucket=self.bucket.name, Key=object_key, ExpiresIn=expires_in)
+            logger.info("Got presigned POST URL: %s", response['url'])
+        except ClientError:
+            logger.exception(
+                "Couldn't get a presigned POST URL for bucket '%s' and object '%s'",
+                self.bucket.name, object_key)
+            raise
+        return response
+
