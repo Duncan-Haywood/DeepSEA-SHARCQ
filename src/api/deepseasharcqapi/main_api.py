5c5fbc2a9b7405839a0c5dfaeaf9953b3bd08754
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-07-05 16:36:40 -0600

    make handler global for lambda use

--- src/api/deepseasharcqapi/main_api.py
+++ src/api/deepseasharcqapi/main_api.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, UploadFile
 from pydantic import BaseModel
 from datetime import datetime
-from typing import List
+from typing import List, Optional
 from mangum import Mangum
 from .aws_util import AWSUtil
 import uvicorn
@@ -11,6 +11,7 @@ from asgiref.typing import ASGIApplication
 class PredictBody(BaseModel):
     user: str
     email: str
+    api_key: Optional[str]
 
 def create_api():
     app = FastAPI()
@@ -24,21 +25,22 @@ def create_api():
         #upload to s3 - TODO might need more parallelization or memory management ie in parts for upload - might need some errror handling for uploads greater than ~7 GB (~175 files) (10 gb max on lambda)
         aws_util.s3_upload_files(files)
         #run ai application and close it when done
-        aws_util.exec_predict_ecs()
+        aws_util.exec_predict_ecs(email=body.email)
 
         return {'results': 'we will email you with a signed url to the  predictions at the email you provided. You can download the result files with this.'} # maybe they need to do a get request at this fast api server when they have the url. TODO
 
     # allows usage on aws lambda
-    handler = Mangum(app)
+    global.handler = Mangum(app)
 
-    return app, handler
+    return app
 
 def run_server(app: ASGIApplication):
     uvicorn.run(app, port=5000, log_level="info")
 
 def main():
-    app, handler = create_api()
+    app = create_api()
     run_server(app)
+    
 
 if __name__ == '__main__':
     main()
