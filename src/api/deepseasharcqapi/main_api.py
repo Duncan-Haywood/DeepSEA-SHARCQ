61d52a914ec9b7bd75d662c826ddbaefcff1befe
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-07-14 10:17:56 -0600

    updates to api code

--- src/api/deepseasharcqapi/main_api.py
+++ src/api/deepseasharcqapi/main_api.py
@@ -12,30 +12,39 @@ class PredictBody(BaseModel):
     email: str
     api_key: Optional[str]
 
-def create_api():
-    app = FastAPI()
+class API: 
+    def __init__(self):
+        self.app = FastAPI() # type: ASGIApplication
+        self.time = str(datetime.now())
+        self.folder_path=None
+        self.aws_util = None
+    def create_api(self):
+        """two paths: post: predict and get: results"""
+        @self.app.post('/predict/')
+        async def predict(files_zip: UploadFile, body: PredictBody):
+            #create folder path and awsUtil obj
+            self.folder_path = f'{body.user}_{self.time}'
+            self.aws_util = AWSUtil(self.folder_path)
 
-    @app.post('/predict/')
-    async def predict(files_zip: UploadFile, body: PredictBody):
-        #create folder path and awsUtil obj
-        time = str(datetime.now())
-        folder_path = f'{body.user}_{time}'
-        aws_util = AWSUtil(folder_path)
-        aws_util.s3_upload_file(files_zip)
-        aws_util.store_user_data(body) #TODO
+            self.aws_util.s3_upload_file(files_zip)
+            self.aws_util.store_user_data(body) #TODO
 
-        return {'results': 'request results in a few minutes after we process your files.'}
-    @app.get('/results/')
-    async def get_results():
-        return NotImplementedError
-    return app
+            return {'results': 'request results in a few minutes after we process your files.'}
 
-def run_server(app: ASGIApplication):
-    uvicorn.run(app, port=8000, log_level="info")
+        @self.app.get('/results/')
+        async def get_results():
+            file = self.aws_util.s3_download_file(self.folder_path)
+            return dict(results=file)
+
+    def run_server(self):
+        uvicorn.run(self.app, port=8000, log_level="info")
+        
+    def main(self):
+        self.create_api()
+        self.run_server()
 
 def main():
-    app = create_api()
-    run_server(app)
+    API().main()
     
 
 if __name__ == '__main__':
