6c7cb3ab974c2457f22cfb60176500b38f1406ea
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-07-21 21:02:39 -0600

    initial outline for ai infra needed

--- src/infrastructure/infrastructure/ai_stack.py
+++ src/infrastructure/infrastructure/ai_stack.py
@@ -7,6 +7,19 @@ class AIStack(Stack):
         scope: Construct,
         id: str,
         prod: bool = False,
+        unzipped_queue = None,
+        results_s3_bucket = None,
         **kwargs,
     ) -> None:
         super().__init__(scope, id, **kwargs)
+
+        # path to dockerfile
+        self.file_path = path.join(path.dirname(__file__), "..", "..", "ai")
+
+        # lambda from dockerfile:
+
+        # add lambda listener to unzipped sqs
+
+        # output s3 bucket - from fastapi stack.
+
+        
