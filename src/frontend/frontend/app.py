ff6f95c785c241bf7499f9ae3e332dec7121c2e5
duncan-haywood <duncanhaywood@appliedmlexperts.com>
2022-07-28 14:15:17 -0600

    updates to frontend

--- src/frontend/frontend/app.py
+++ src/frontend/frontend/app.py
@@ -1,14 +1,31 @@
-from dash import Dash
-import dash_auth
-
+from dash import Dash, dcc, html
 from flask import Flask
-from dash_cognito_auth import CognitoOAuth
 
 def main():
-    server = Flask(__name__)
-    app = Dash(__name__, server=server, url_base_pathname='/')
-    auth = CognitoOAuth(app)
+    app = Dash(__name__)
+    server = app.server
+    app.layout = html.Div([
+        html.H1("DeepSea-Sharcq"),
+        html.P("upload your zipped folders of files you want to process"),
+        html.P("please keep your folder (pre-zip) under 8 gb or the application will fail, and you won't get results. We set file limit at approximately 4 gb since zip should cut file size in half"),
+        html.P("it takes a few minutes for the ai to process all files"),
+        dcc.Upload(html.Button("Upload zip folder"), max_size=4*10**9, filetypes=['zip']),
+        html.Button("Download results", id="download_button"),
+        dcc.Download(children="download_button")
+    ])
+    def upload_zip_folder(zip_folder):
+        # url = requests.post(self.rest_api_url/upload_url, username, api_key, "POST")
+        # response = request.post(url, zip_folder)
+        pass
+
+    def download_results():
+        # url = requests.post(f'{self.rest_api_url}/download_url', username, api_key, "GET")
+        # response = request.post(url, zip_folder)        
+        pass
+    return app
     
 
 if __name__=='__main__':
-    main()
+    app = main()
+    app.run_server(debug=True)
+
